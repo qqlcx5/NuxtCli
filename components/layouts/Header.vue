757c00362b7bdeecff0648103fe08a68f156d0c4
another <893917884@qq.com>
2020-10-19 14:58:11 +0800

    img标签间距问题

--- components/layouts/Header.vue
+++ components/layouts/Header.vue
@@ -2,7 +2,6 @@
     <header>
         <el-menu
             :default-active="activeIndex"
-            class="el-menu-demo"
             mode="horizontal"
             background-color="#545c64"
             text-color="#fff"
